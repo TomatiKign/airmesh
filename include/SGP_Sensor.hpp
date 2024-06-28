fb44b4818dff252fc54e52164d5c4a8a05ae833b
Edison <zhouedison353@gmail.com>
2024-06-28 10:26:45 -0400

    ADDED SGP function

--- include/SGP_Sensor.hpp
+++ include/SGP_Sensor.hpp
@@ -30,16 +30,19 @@ void StartSGP(){
 
 void sgpLoop() {
   int32_t voc_index;
-bme.setTemperatureOversampling(BME680_OS_8X);
-    Temp = bme.temperature;
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  Temp = bme.temperature;
   bme.setHumidityOversampling(BME680_OS_2X);
-
-    Humidity = bme.humidity;
- Serial.print("Temp *C = "); Serial.print(bme.temperature); Serial.print("\t\t");
-   Serial.print("Hum. % = "); Serial.println(bme.humidity);
+  Humidity = bme.humidity;
   voc_index = sgp.measureVocIndex(Temp, Humidity);
   Serial.print("Voc Index: ");
   Serial.println(voc_index);
-  delay(1000);
 }
 
+float returnVoc(){
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  Temp = bme.temperature;
+  bme.setHumidityOversampling(BME680_OS_2X);
+  Humidity = bme.humidity;
+  return sgp.measureVocIndex(Temp, Humidity);
+}
